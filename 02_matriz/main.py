9b0eb8ae4c99d9148497ec236747f2fc064bc06f
Flavio Coscarella <flaviodc@live.com.ar>
2022-12-08 08:03:49 -0300

    Refactorización y debugging

--- 02_matriz/main.py
+++ 02_matriz/main.py
@@ -1,19 +1,27 @@
 import numpy.random as nprandom
 
+"Size siempre debería ser al >= 4 para que no existan errores"
+size = 5
+rangoi = size - 3
+maxnumber = 5
+
 
 def crearmatriz():
-    sizeheight = 5
-    sizewidth = 5
-    maxnumber = 5
-    matrizcreada = nprandom.randint(maxnumber, size=(sizeheight, sizewidth))
-    return matrizcreada
+    return nprandom.randint(maxnumber, size=(size, size))
 
 
 def numerosconsecutivos(mat):
     """Para simplificar, se realiza todo en un solo bucle anidado for, al comparar 4 números al mismo tiempo
     no es necesario guardar y comparar varias veces"""
-    for i in range(2):
-        for j in range(5):
+    print(mat)
+    matrizprueba = [[4, 3, 0, 2, 3],
+                    [0, 0, 1, 2, 8],
+                    [1, 0, 3, 1, 7],
+                    [3, 2, 1, 0, 6],
+                    [0, 2, 3, 4, 5]]
+
+    for i in range(rangoi):
+        for j in range(size):
             conditionvert = mat[j][i] + 3 == mat[j][i + 1] + 2 == mat[j][i + 2] + 1 == mat[j][i + 3]
             if conditionvert:
                 print(f"Existen números consecutivos en forma vertical desde:"
@@ -21,7 +29,7 @@ def numerosconsecutivos(mat):
             conditionvertinversa = mat[j][i] == mat[j][i + 1] + 1 == mat[j][i + 2] + 2 == mat[j][i + 3] + 3
             if conditionvertinversa:
                 print(f"Existen números consecutivos en forma vertical inversa desde: "
-                      f"x={i} y={j + 3}  hasta: x={i} y={j}")
+                      f"x={j} y={i + 3}  hasta: x={j} y={i}")
             conditionhorizontal = mat[i][j] + 3 == mat[i + 1][j] + 2 == mat[i + 2][j] + 1 == mat[i + 3][j]
             if conditionhorizontal:
                 print(f"Existen números consecutivos en forma horizontal desde:"
