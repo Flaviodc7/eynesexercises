08c314b1277807af6b4b0c83dab5d1aee53df59e
Flavio Coscarella <flaviodc@live.com.ar>
2022-12-08 08:29:50 -0300

    Creando clase Circulo

--- 03_clases/main.py
+++ 03_clases/main.py
@@ -1,1 +1,11 @@
-class circulo:
+class Circulo:
+    pi = 3.14
+
+    def __init__(self, radio):
+        self._radio = radio
+
+    def area(self):
+        return f'El area del circulo es: {self.pi * pow(self._radio, 2)}'
+
+    def perimetro(self):
+        return f'El perimetro del circulo es: {2 * self.pi * self._radio}'
